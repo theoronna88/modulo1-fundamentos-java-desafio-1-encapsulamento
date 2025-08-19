b720f0db72676d5459677de7b2fd1b8f21651451
theoronna88 <theo@ronna.com.br>
2025-08-19 19:19:39 -0300

    fix: update Produto Class to encapsulate attributes, and change on main to call the setters methods

--- src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
+++ src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
@@ -3,9 +3,9 @@ package br.com.unipds.fundamentosjava.desafio;
 public class Produto {
 
     public int id;
-    public String nome;
-    public double preco;
-    public int quantidadeEmEstoque;
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
 
     public Produto(int id, String nome, double preco, int quantidadeEmEstoque) {
         this.id = id;
@@ -14,8 +14,46 @@ public class Produto {
         this.quantidadeEmEstoque = quantidadeEmEstoque;
     }
 
+
+
     @Override
     public String toString() {
         return "Produto [ID=" + id + ", Nome=" + nome + ", Preço=" + preco + ", Estoque=" + quantidadeEmEstoque + "]";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            System.out.println("Preço inválido: O preço não pode ser negativo.");
+            return;
+        }
+        if (preco == 0) {
+            System.out.println("Preço inválido: O preço não pode ser zero.");
+            return;
+        }
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        if (quantidadeEmEstoque < 0) {
+            System.out.println("Quantidade inválida: O estoque não pode ser negativo.");
+            return;
+        }
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
     }
 }
